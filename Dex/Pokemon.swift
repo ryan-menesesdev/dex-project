b24cf907b42b1b2d2e85306b948f318cc9893973
Ryan Meneses <ryanmeneses.dev@gmail.com>
2025-08-28 10:30:44 -0300

    add: Model file created on Swift Data standards

--- Dex/Pokemon.swift
+++ Dex/Pokemon.swift
@@ -1,17 +1,31 @@
-import Foundation
+//
+//  Pokemon.swift
+//  Dex
+//
+//  Created by Ryan Davi Oliveira de Meneses on 28/08/25.
+//
+//
 
-struct FetchedPokemon: Decodable {
-    let id: Int16
-    let name: String
-    let types: [String]
-    let hp: Int16
-    let attack: Int16
-    let specialAttack: Int16
-    let defense: Int16
-    let specialDefense: Int16
-    let speed: Int16
-    let spriteURL: URL
-    let shinyURL: URL
+import Foundation
+import SwiftData
+import SwiftUI
+
+@Model public class Pokemon: Decodable {
+
+    @Attribute(.unique) var id: Int
+    var name: String
+    var types: [String]
+    var hp: Int
+    var attack: Int
+    var defense: Int
+    var specialAttack: Int
+    var specialDefense: Int
+    var speed: Int
+    var sprite: Data?
+    var shiny: Data?
+    var spriteURL: URL
+    var shinyURL: URL
+    var favorite: Bool = false
     
     enum CodingKeys: CodingKey {
         case id
@@ -38,7 +52,7 @@ struct FetchedPokemon: Decodable {
         }
     }
     
-    init(from decoder: any Decoder) throws {
+    public required init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.id = try container.decode(Int16.self, forKey: .id)
         self.name = try container.decode(String.self, forKey: .name)
@@ -79,4 +93,67 @@ struct FetchedPokemon: Decodable {
         self.spriteURL = try spriteContainer.decode(URL.self, forKey: .spriteURL)
         self.shinyURL = try spriteContainer.decode(URL.self, forKey: .shinyURL)
     }
+    
+    var spriteImage: Image {
+        if let data = sprite, let image = UIImage(data: data) {
+            Image(uiImage: image)
+        } else {
+            Image(.bulbasaur)
+        }
+    }
+    
+    var shinyImage: Image {
+        if let data = shiny, let image = UIImage(data: data) {
+            Image(uiImage: image)
+        } else {
+            Image(.shinybulbasaur)
+        }
+    }
+    
+    var background: ImageResource {
+        switch types[0] {
+        case "rock", "ground", "steel", "fighting", "ghost", "dark", "psychic":
+                .rockgroundsteelfightingghostdarkpsychic
+            
+        case "fire", "dragon":
+                .firedragon
+            
+        case "flying", "bug":
+                .flyingbug
+            
+        case "ice":
+                .ice
+            
+        case "water" :
+                .water
+        default:
+                .normalgrasselectricpoisonfairy
+        }
+    }
+    
+    var typeColor: Color {
+        return Color(types[0].capitalized)
+    }
+    
+    var stats: [Stat] {
+        [
+            Stat(id: 1, name: "HP", value: hp),
+            Stat(id: 2, name: "Attack", value: attack),
+            Stat(id: 3, name: "Defense", value: defense),
+            Stat(id: 4, name: "Special Attack", value: specialAttack),
+            Stat(id: 5, name: "Special Defense", value: specialDefense),
+            Stat(id: 6, name: "Speed", value: speed)
+        ]
+    }
+    
+    var highestValue: Stat {
+        stats.max { $0.value < $1.value }!
+    }
+    
+    struct Stat: Identifiable {
+        let id: Int
+        let name: String
+        let value: Int
+    }
+    
 }
